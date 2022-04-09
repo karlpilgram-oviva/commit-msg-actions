705ee45f00a73f31214c46b42486181766e7de6b
Karl Pilgram <95346191+karlpilgram-oviva@users.noreply.github.com>
2022-04-09 13:12:19 +0200

    ABC-123: Commit message C

--- 01helloWorld/main.go
+++ 01helloWorld/main.go
@@ -4,8 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("print some string")
-	fmt.Println("print some string")
-	fmt.Println("print some string")
-	fmt.Println("print some string")
-	fmt.Println("print some string")
 }
